cd8b7316c3ef428439d49dee09e91bf12cac0d28
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-07-30 20:18:30 +0300

    Создал первое прилождение

--- Examples__001/Program.cs
+++ Examples__001/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// сделаем приветствие в нашей программе
+Console.WriteLine ("Привет давайте познакомимся поближе, как вас зовут?");
+string username=Console.ReadLine();
+Console.WriteLine ("Привет, добро пожаловать на наш проект");
+Console.WriteLine (username);
+Console.WriteLine ( "Ну чтоже я вам хочу пожелать вам удачи, грызите науку! Рад знакомству");
