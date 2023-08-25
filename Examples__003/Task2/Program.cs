24c7a39a440fe72af55c814f1a0a8f358bce74cd
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-25 13:54:31 +0300

    save3

--- Examples__003/Task2/Program.cs
+++ Examples__003/Task2/Program.cs
@@ -1,15 +1,15 @@
-﻿string Methode4(int count, string text)
+﻿string Methode1(int count, string text)
 {
     int i = 0; // возьмем цикл
     string result = String.Empty;
-    
-    while ( i < count)
+      
+    for( int i = 0; i <= count; i++)
     {
         result = result + text;
-        i++;
+        // i++  убираем так как создали  for( int i = 0; < count; i++)
     }
     return result; //завершаем
 }
 
-string res = Methode4(10, "QWERTY") // Нужно создать переменную и указать количество
-System.WriteLine(res) // Чтобы показать на эране какой результат, выводим консоль
+string res = Methode1 (10, "qwerty"); // Нужно создать переменную и указать количество
+System.WriteLine(res); // Чтобы показать на эране какой результат, выводим консоль
--- Examples__003/Task3/Program.cs
+++ Examples__003/Task3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i = 0; // возьмем цикл
+    string result = String.Empty;
+      
+    for( int i = 0; i <= count; i++)
+    {
+        result = result + text;
+        // i++  убираем так как создали  for( int i = 0; < count; i++)
+    }
+    return result; //завершаем
