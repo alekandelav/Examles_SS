7df1df812681800538995963a39840c409596b9d
alekandelav <140648372+AleksandrLAV@users.noreply.github.com>
2023-08-25 13:37:02 +0300

    all

--- Examples__003/Task1/Program.cs
+++ Examples__003/Task1/Program.cs
@@ -1,4 +1,5 @@
-﻿//вид 1
+﻿
+//вид 1
 
 void Method()
 {
@@ -23,4 +24,37 @@ void Method21(string msg, int count)
     i++;  //если мы оставим count++ будет увеличиваться много раз надо поставить -> i++
     }
 }
-Methode21("Текст", 4); //вводим 4 чтобы открыть сообщение 4 раза
+//Methode21("Текст", 4); //способ 1: вводим 4 чтобы открыть сообщение 4 раза
+// Methode21(count: 4, msg: "Новый текст"); //2 пособ написать так наш метод с обозначением
+
+
+//Вид 3
+
+int Methode3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Methode3();
+//Console.WriteLine(year);
+
+
+
+
+//Вид 4 - принимает и возвращает
+
+string Methode4(int count, string text)
+{
+    int i = 0; // возьмем цикл
+    string result = String.Empty;
+    
+    while ( i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result; //завершаем
+}
+
+string res = Methode4(10, "QWERTY") // Нужно создать переменную и указать количество
+System.WriteLine(res) // Чтобы показать на эране какой результат, выводим консоль.
--- Examples__003/Task2/Program.cs
+++ Examples__003/Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Methode4(int count, string text)
+{
+    int i = 0; // возьмем цикл
+    string result = String.Empty;
+    
+    while ( i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result; //завершаем
+}
+
+string res = Methode4(10, "QWERTY") // Нужно создать переменную и указать количество
+System.WriteLine(res) // Чтобы показать на эране какой результат, выводим консоль
